ed0a210ab52e5850c628fc4b7797c00031463b6f
Nam Duong <phamnamduong583@gmail.com>
2025-01-08 22:48:58 +0700

    Tạo http server với GIN

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"net/http"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 type TodoItem struct {
@@ -25,21 +26,29 @@ func main() {
 		Description: "Learn Go programming language",
 		Status:      "Doing",
 		CreatedAt:   &now,
-		UpdatedAt:   nil,
+		UpdatedAt:   &now,
 	}
-	jsonData, err := json.Marshal(item)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(string(jsonData))
+	// jsonData, err := json.Marshal(item)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println(string(jsonData))
 
-	jsonStr := "{\"id\":1,\"title\":\"Learn Go\",\"description\":\"Learn Go programming language\",\"status\":\"Doing\",\"created_at\":\"2021-09-01T00:00:00Z\",\"updated_at\":null}"
-	var item2 TodoItem
-	err = json.Unmarshal([]byte(jsonStr), &item2)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(item2)
+	// jsonStr := "{\"id\":1,\"title\":\"Learn Go\",\"description\":\"Learn Go programming language\",\"status\":\"Doing\",\"created_at\":\"2021-09-01T00:00:00Z\",\"updated_at\":null}"
+	// var item2 TodoItem
+	// err = json.Unmarshal([]byte(jsonStr), &item2)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println(item2)
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": item,
+		})
+	})
+	r.Run()
 }
